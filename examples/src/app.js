c30dd44f972f4912bbcd4ec698ad39d95754de47
zhangkun <1766597067@qq.com>
2018-12-13 18:44:23 +0800

    create

---
+++ examples/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'rare-mobile/style/component.css';// eslint-disable-line
+import Block from 'rare-mobile/src'; // eslint-disable-line
+
+ReactDOM.render(
+  <div className="component-template">
+    <Block />
+  </div>,
+  document.getElementById('example'),
+);
