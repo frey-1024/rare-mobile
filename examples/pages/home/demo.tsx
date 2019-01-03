6f19c59eb73170411eea797f8cab23c6d2681d21
zhangkun <1766597067@qq.com>
2019-01-03 18:10:24 +0800

    demo 路由配置

--- examples/pages/home/demo.tsx
+++ examples/pages/home/demo.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import {demoList} from './utils/menu';
+import './styles/demo.scss';
 
-export default class PullToRefreshNormalDemo extends React.Component {
+export default class HomeDemo extends React.Component {
   render() {
-    return 'mobile';
+    return <div className="home-demo">
+      {
+        demoList.map((item: any) => {
+          return <Fragment key={item.title}>
+            <h3 className="home-list-title">{item.title}</h3>
+            {item.children.map((child: any) => {
+              return <Link to={`/demo${child.path}`} className="home-child-list" key={child.title}>
+                <p className="child-list-title">{child.title}</p>
+                <p className="child-list-description">{child.description}</p>
+              </Link>;
+            })}
+          </Fragment>;
+        })
+      }
+    </div>;
   }
 }
